238bea3acb7d34ad516b76e96828c45833c53011
konopskyyy <konopskyyy@gmail.com>
2025-08-08 22:53:47 +0200

    add: authorization for admin panel

---
+++ src/User/UserInterface/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\User\UserInterface\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $this->logger->info('Logowanie: próba logowania', [
+            'last_username' => $lastUsername,
+            'error' => $error ? $error->getMessageKey() : null,
+        ]);
+
+        return $this->render('security/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
