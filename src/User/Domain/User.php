da4531e835030d2d632fa68fa7e2680f89f37cea
konopskyyy <konopskyyy@gmail.com>
2025-10-04 21:36:27 +0200

    fixy

--- src/User/Domain/User.php
+++ src/User/Domain/User.php
@@ -60,7 +60,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return $this->email ?? '';
+        return $this->email ?? 'empty';
     }
 
     /**
