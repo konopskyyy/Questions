9ac3fe7a1286da6333129982f810e29ca5b1e497
konopskyyy <konopskyyy@gmail.com>
2026-02-07 12:20:44 +0100

    add: swagger

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
